49a88ce0fde1388f4555930abcc3cf5a1781e179
Anastasija <anastasija.mandic2003@gmail.com>
2026-09-11 23:28:55 +0200

    Add lighting controls and timed cinema event

--- app/resources/shaders/basic.glsl
+++ app/resources/shaders/basic.glsl
@@ -1,48 +1,114 @@
-// #shader vertex
+//#shader vertex
 
 #version 330 core
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+out vec3 FragPos;
 out vec3 Normal;
+out vec2 TexCoords;
 
 void main()
 {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
-    Normal = aNormal;
+    FragPos = vec3(model * vec4(aPos, 1.0));
+
+    Normal =
+        mat3(transpose(inverse(model))) * aNormal;
+
+    TexCoords = aTexCoords;
+
+    gl_Position =
+        projection * view * vec4(FragPos, 1.0);
 }
 
 
-// #shader fragment
+//#shader fragment
 
 #version 330 core
 
+in vec3 FragPos;
 in vec3 Normal;
+in vec2 TexCoords;
 
 out vec4 FragColor;
 
+uniform vec3 objectColor;
+
+uniform sampler2D texture_diffuse1;
+uniform bool hasDiffuseTexture;
+
+uniform vec3 directionalDirection;
+uniform vec3 directionalColor;
+
+uniform vec3 pointPosition;
+uniform vec3 pointColor;
+
 void main()
 {
-    vec3 n = normalize(Normal);
+    vec3 normal = normalize(Normal);
 
-    // Pod - srednje siv
-    if (n.y > 0.5)
+    vec3 baseColor;
+
+    if (hasDiffuseTexture)
     {
-        FragColor = vec4(0.55, 0.55, 0.55, 1.0);
+        baseColor = texture(texture_diffuse1, TexCoords).rgb;
     }
-    // Zadnji zid - svetao
-    else if (n.z > 0.5)
-    {
-        FragColor = vec4(0.9, 0.9, 0.9, 1.0);
-    }
-    // Levi i desni zid - tamnosivi
     else
     {
-        FragColor = vec4(0.3, 0.3, 0.3, 1.0);
+        baseColor = objectColor;
     }
+
+    // Ambient
+    vec3 ambient =
+        0.25 * baseColor;
+
+    // Directional light
+    vec3 directionalDir =
+        normalize(-directionalDirection);
+
+    float directionalStrength =
+        max(dot(normal, directionalDir), 0.0);
+
+    vec3 directional =
+        directionalStrength *
+        directionalColor *
+        baseColor;
+
+    // Point light
+    vec3 pointDir =
+        normalize(pointPosition - FragPos);
+
+    float pointStrength =
+        max(dot(normal, pointDir), 0.0);
+
+    float distance =
+        length(pointPosition - FragPos);
+
+    float attenuation =
+        1.0 /
+        (
+            1.0 +
+            0.09 * distance +
+            0.032 * distance * distance
+        );
+
+    vec3 point =
+        pointStrength *
+        pointColor *
+        baseColor *
+        attenuation;
+
+    vec3 result =
+        ambient +
+        directional +
+        point;
+
+    FragColor =
+        vec4(result, 1.0);
 }
--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -7,6 +7,7 @@
 #include <engine/resources/Model.hpp>
 #include <engine/resources/Shader.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <chrono>
 
 class CinemaController final : public engine::core::Controller {
 public:
@@ -48,6 +49,71 @@ public:
         if (platform->key(engine::platform::KEY_ESCAPE).state() ==
             engine::platform::Key::State::JustPressed) { return false; }
 
+        // 1 - smanji directional light
+        if (platform->key(engine::platform::KEY_1).state() ==
+            engine::platform::Key::State::JustPressed) {
+            directionalIntensity -= 0.1f;
+
+            if (directionalIntensity < 0.0f) directionalIntensity = 0.0f;
+        }
+
+        // 2 - pojacaj directional light
+        if (platform->key(engine::platform::KEY_2).state() ==
+            engine::platform::Key::State::JustPressed) {
+            directionalIntensity += 0.1f;
+
+            if (directionalIntensity > 2.0f) directionalIntensity = 2.0f;
+        }
+
+        // 3 - smanji point light
+        if (platform->key(engine::platform::KEY_3).state() ==
+            engine::platform::Key::State::JustPressed) {
+            pointIntensity -= 0.1f;
+
+            if (pointIntensity < 0.0f) pointIntensity = 0.0f;
+        }
+
+        // 4 - pojacaj point light
+        if (platform->key(engine::platform::KEY_4).state() ==
+            engine::platform::Key::State::JustPressed) {
+            pointIntensity += 0.1f;
+
+            if (pointIntensity > 2.0f) pointIntensity = 2.0f;
+        }
+
+        // P - pokreni cinema dogadjaj
+        if (platform->key(engine::platform::KEY_P).state() ==
+            engine::platform::Key::State::JustPressed) {
+            cinemaEventStarted = true;
+            lightsDimmed = false;
+            screenBrightened = false;
+
+            eventStartTime = std::chrono::steady_clock::now();
+        }
+
+        if (cinemaEventStarted) {
+            auto now = std::chrono::steady_clock::now();
+
+            float elapsed =
+                    std::chrono::duration<float>(now - eventStartTime).count();
+
+            // Posle 2 sekunde prigusi svetla
+            if (elapsed >= 2.0f && !lightsDimmed) {
+                directionalIntensity = 0.2f;
+                pointIntensity = 0.2f;
+
+                lightsDimmed = true;
+            }
+
+            // Posle ukupno 4 sekunde osvetli platno
+            if (elapsed >= 4.0f && !screenBrightened) {
+                screenBrightness = 2.0f;
+
+                screenBrightened = true;
+                cinemaEventStarted = false;
+            }
+        }
+
         return true;
     }
 
@@ -61,6 +127,28 @@ public:
 
         shader->use();
 
+        // Directional light
+        shader->set_vec3(
+                "directionalDirection",
+                glm::vec3(-0.2f, -1.0f, -0.3f)
+                );
+
+
+        shader->set_vec3(
+                "directionalColor",
+                glm::vec3(0.45f, 0.45f, 0.50f) * directionalIntensity
+                );
+
+        // Point light - lampa
+        shader->set_vec3(
+                "pointPosition",
+                glm::vec3(-3.0f, 3.0f, 2.5f)
+                );
+
+        shader->set_vec3(
+                "pointColor",
+                glm::vec3(1.0f, 0.80f, 0.55f) * pointIntensity
+                );
         // Projection matrica
         shader->set_mat4(
                 "projection",
@@ -78,8 +166,10 @@ public:
         glm::mat4 model = glm::mat4(1.0f);
 
         shader->set_mat4("model", model);
-
+        shader->set_vec3("objectColor", glm::vec3(0.4f, 0.4f, 0.4f));
+        shader->set_bool("hasDiffuseTexture", true);
         room->draw(shader);
+        shader->set_bool("hasDiffuseTexture", false);
 
         glm::mat4 sofa_model = glm::mat4(1.0f);
 
@@ -99,14 +189,18 @@ public:
                 glm::vec3(0.05f)
                 );
 
+        shader->set_bool("hasDiffuseTexture", true);
+        shader->set_vec3("objectColor", glm::vec3(0.12f, 0.08f, 0.06f));
+
         shader->set_mat4("model", sofa_model);
         sofa->draw(shader);
+        shader->set_bool("hasDiffuseTexture", false);
 
         glm::mat4 platno_model = glm::mat4(1.0f);
 
         platno_model = glm::translate(
                 platno_model,
-                glm::vec3(5.0f, 2.2f, 0.0f)
+                glm::vec3(5.0f, 2.2f, 5.0f)
                 );
 
         platno_model = glm::rotate(
@@ -121,7 +215,14 @@ public:
                 );
 
         shader->set_mat4("model", platno_model);
+
+        shader->set_vec3(
+                "objectColor",
+                glm::vec3(1.0f, 1.0f, 1.0f) * screenBrightness
+                );
+
         platno->draw(shader);
+
         glm::mat4 table_model = glm::mat4(1.0f);
 
         table_model = glm::translate(
@@ -135,7 +236,19 @@ public:
                 );
 
         shader->set_mat4("model", table_model);
+
+        // Sto koristi svoju albedo teksturu
+        shader->set_bool("hasDiffuseTexture", true);
+
+        shader->set_vec3(
+                "objectColor",
+                glm::vec3(0.25f, 0.15f, 0.08f)
+                );
+
         table->draw(shader);
+
+        // Sledeci objekti trenutno nemaju diffuse teksturu
+        shader->set_bool("hasDiffuseTexture", false);
 
         glm::mat4 projector_model = glm::mat4(1.0f);
 
@@ -145,6 +258,12 @@ public:
                 );
 
         shader->set_mat4("model", projector_model);
+
+        shader->set_vec3(
+                "objectColor",
+                glm::vec3(0.65f, 0.65f, 0.65f)
+                );
+
         projector->draw(shader);
 
         glm::mat4 lamp_model = glm::mat4(1.0f);
@@ -160,7 +279,19 @@ public:
                 );
 
         shader->set_mat4("model", lamp_model);
+
+        // Lampa koristi svoju teksturu
+        shader->set_bool("hasDiffuseTexture", true);
+
+        shader->set_vec3(
+                "objectColor",
+                glm::vec3(0.55f, 0.45f, 0.30f)
+                );
+
         lamp->draw(shader);
+
+        // Sledeći objekti nemaju diffuse teksturu
+        shader->set_bool("hasDiffuseTexture", false);
     }
 
     void end_draw() override {
@@ -180,6 +311,16 @@ private:
     engine::resources::Model *projector{};
     engine::resources::Model *lamp{};
     engine::resources::Shader *shader{};
+    float directionalIntensity = 1.0f;
+    float pointIntensity = 1.0f;
+
+    bool cinemaEventStarted = false;
+    bool lightsDimmed = false;
+    bool screenBrightened = false;
+
+    std::chrono::steady_clock::time_point eventStartTime;
+
+    float screenBrightness = 1.0f;
 };
 
 class CinemaApp final : public engine::core::App {
